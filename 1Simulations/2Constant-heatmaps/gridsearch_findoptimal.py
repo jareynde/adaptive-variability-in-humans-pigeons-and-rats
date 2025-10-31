a472819446fa75a68d9056213c9bcf93ff94644d
jareynde <118723557+jareynde@users.noreply.github.com>
2025-10-31 15:17:02 +0100

    Add files via upload

---
+++ 1Simulations/2Constant-heatmaps/gridsearch_findoptimal.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Same as 2Constant_gridsearch-findoptimal.py script in Project1
+
+Rescorla Wagner model with epsilon-greedy decision making policy
+Constant model
+Contexts:
+*Stable: reward probabiliies stay the same during all trials
+*Volatile: reward probabilities shuffle every couple of trials
+*Reinforced variability: reward dependent on how variable options are chosen according to Hide and Seek game
+First reward schedule test:
+    stable: [0.7,0.7,0.7,0.3,0.3,0.3,0.3,0.3]
+    volatile: [0.9,0.9,0.9,0.1,0.1,0.1,0.1,0.1]
+    adversarial: least frequent 60% of sequences
+            
+Critical parameters: epsilon, learning rate and unchosen addition.
+This script will:
+Take results of the gridsearch and find the highest reward that was obtained and the parameter values that did it.
+Also find the optimal parameter for each of our other two 'common sense' values, i.e.:
+* optimal eps when lr=0.25 and lambda=0
+* optimal lr when eps=0.3 and lambda=0
+* optimal lambda when eps=0.3 and lr=0.25
+
+EXACTLY THE SAME AS THE OTHER 2_constant_heatmap.py
+@author: Janne Reynders; janne.reynders@ugent.be
+"""
+import numpy as np                  
+import pandas as pd                 
+import matplotlib.pyplot as plt     
+import os
+import random
+from collections import Counter
+import scipy.stats as stats
+import itertools
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(script_dir, 'results/final/reward')
+heat = 51
+
+all_eps = np.linspace(0, 1, heat, endpoint=True)
+all_lr = np.linspace(0, 1, heat, endpoint=True)
+all_unchosen = np.linspace(-1/8, 1/8, heat, endpoint=True)
+print(all_unchosen)
+#Take results of the gridsearch and find the highest reward that was obtained and the parameter values that did it.
+
+
+max_stable_rewards = np.zeros([heat, 4], dtype=float) #for every eps value there is one row, columns contain: max reward value, eps, learning rate, lambda
+max_volatile_rewards = np.zeros([heat, 4], dtype=float) #for every eps value there is one row, columns contain: max reward value, eps, learning rate, lambda
+max_adversarial_rewards = np.zeros([heat, 4], dtype=float) #for every eps value there is one row, columns contain: max reward value, eps, learning rate, lambda
+
+
+for i in range(heat):
+    advtitle = f'mr_var_eps{i}.csv'
+    adv_dic = os.path.join(data_dir,advtitle)
+    statitle = f'mr_sta_eps{i}.csv'
+    sta_dic = os.path.join(data_dir,statitle)
+    voltitle = f'mr_vol_eps{i}.csv'
+    vol_dic = os.path.join(data_dir,voltitle)
+
+    advdata = pd.read_csv(adv_dic)
+    advdata = advdata.to_numpy()  
+    advdata = advdata[:,3:]
+    advdata = advdata[2:,:]
+
+    stadata = pd.read_csv(sta_dic)
+    stadata = stadata.to_numpy()  
+    stadata = stadata[:,3:]
+    stadata = stadata[2:,:]
+
+    voldata = pd.read_csv(vol_dic)
+    voldata = voldata.to_numpy()
+    voldata = voldata[:,3:]
+    voldata = voldata[2:,:]
+
+    print(np.shape(advdata), np.shape(stadata), np.shape(voldata))
+
+
+    advmax = np.max(advdata)
+    advmaxi = np.unravel_index(np.argmax(advdata), advdata.shape)
+    max_adversarial_rewards[i,0] = advmax
+    max_adversarial_rewards[i,1] = all_eps[i]
+    max_adversarial_rewards[i,2] = all_lr[advmaxi[0]]
+    max_adversarial_rewards[i,3] = all_unchosen[advmaxi[1]]
+
+
+    stamax = np.max(stadata)
+    stamaxi = np.unravel_index(np.argmax(stadata), stadata.shape)
+    max_stable_rewards[i,0] = stamax
+    max_stable_rewards[i,1] = all_eps[i]
+    max_stable_rewards[i,2] = all_lr[stamaxi[0]]
+    max_stable_rewards[i,3] = all_unchosen[stamaxi[1]]
+
+    
+    volmax = np.max(voldata)
+    volmaxi = np.unravel_index(np.argmax(voldata), voldata.shape)
+    max_volatile_rewards[i,0] = volmax
+    max_volatile_rewards[i,1] = all_eps[i]
+    max_volatile_rewards[i,2] = all_lr[volmaxi[0]]
+    max_volatile_rewards[i,3] = all_unchosen[volmaxi[1]]
+
+
+
+print(advdata)
+print(max_volatile_rewards)
+advmax2 = np.max(max_adversarial_rewards[:,0])
+advmaxi2 = np.argmax(max_adversarial_rewards[:,0])
+advmax_eps = max_adversarial_rewards[advmaxi2,1]
+advmax_lr = max_adversarial_rewards[advmaxi2,2]
+advmax_lambda = max_adversarial_rewards[advmaxi2,3]
+
+print('the highest reward in adversarial is', advmax2, 'where eps is', advmax_eps, 'learning rate is', advmax_lr, 'and lambda is', advmax_lambda ) 
+
+
+stamax2 = np.max(max_stable_rewards[:,0])
+stamaxi2 = np.argmax(max_stable_rewards[:,0])
+stamax_eps = max_stable_rewards[stamaxi2,1]
+stamax_lr = max_stable_rewards[stamaxi2,2]
+stamax_lambda = max_stable_rewards[stamaxi2,3]
+
+print('the highest reward in stable is', stamax2, 'where eps is', stamax_eps, 'learning rate is', stamax_lr, 'and lambda is', stamax_lambda ) 
+
+
+
+
+volmax2 = np.max(max_volatile_rewards[:,0])
+volmaxi2 = np.argmax(max_volatile_rewards[:,0])
+volmax_eps = max_volatile_rewards[volmaxi2,1]
+volmax_lr = max_volatile_rewards[volmaxi2,2]
+volmax_lambda = max_volatile_rewards[volmaxi2,3]
+
+print('the highest reward in volatile is', volmax2, 'where eps is', volmax_eps, 'learning rate is', volmax_lr, 'and lambda is', volmax_lambda ) 
+
+'''
+
+#Find the optimal parameter for each of our other two 'common sense' values
+
+lr_index = np.where(all_lr == 0.24)[0][0]
+lambda_index = np.where(all_unchosen == 0)[0][0]
+eps_index = np.where(all_eps == 0.3)[0][0]
+
+MLeps = np.zeros([heat, 3]) #3 columns, one for each environment
+for i in range(heat):
+    advtitle = f'adv_mr_eps{i}.xlsx'
+    adv_dic = os.path.join(data_dir,advtitle)
+    statitle = f'sta_mr_eps{i}.xlsx'
+    sta_dic = os.path.join(data_dir,statitle)
+    voltitle = f'vol_mr_eps{i}.xlsx'
+    vol_dic = os.path.join(data_dir,voltitle)
+
+    advdata = pd.read_excel(adv_dic, engine="openpyxl")
+    advdata = advdata.to_numpy()  
+    advdata = advdata[:,1:]
+
+    stadata = pd.read_excel(sta_dic, engine="openpyxl")
+    stadata = stadata.to_numpy()  
+    stadata = stadata[:,1:]
+
+    voldata = pd.read_excel(vol_dic, engine="openpyxl")
+    voldata = voldata.to_numpy()
+    voldata = voldata[:,1:]
+
+    MLeps[i,0] = advdata[lr_index,lambda_index]
+    MLeps[i,1] = stadata[lr_index,lambda_index]
+    MLeps[i,2] = voldata[lr_index,lambda_index]
+
+
+   
+
+
+maxadv_epsi = np.argmax(MLeps[:,0])
+maxadv_eps = all_eps[maxadv_epsi]
+
+maxsta_epsi = np.argmax(MLeps[:,1])
+maxsta_eps = all_eps[maxsta_epsi]
+
+maxvol_epsi = np.argmax(MLeps[:,2])
+maxvol_eps = all_eps[maxvol_epsi]
+
+
+advtitle = f'adv_mr_eps{eps_index}.xlsx'
+adv_dic = os.path.join(data_dir,advtitle)
+statitle = f'sta_mr_eps{eps_index}.xlsx'
+sta_dic = os.path.join(data_dir,statitle)
+voltitle = f'vol_mr_eps{eps_index}.xlsx'
+vol_dic = os.path.join(data_dir,voltitle)
+advdata = pd.read_excel(adv_dic, engine="openpyxl")
+advdata = advdata.to_numpy()  
+advdata = advdata[:,1:]
+
+stadata = pd.read_excel(sta_dic, engine="openpyxl")
+stadata = stadata.to_numpy()  
+stadata = stadata[:,1:]
+
+voldata = pd.read_excel(vol_dic, engine="openpyxl")
+voldata = voldata.to_numpy()
+voldata = voldata[:,1:]
+
+
+
+maxadv_lri = np.argmax(advdata[:,lambda_index])
+maxadv_lr = all_lr[maxadv_lri]
+
+maxsta_lri = np.argmax(stadata[:,lambda_index])
+maxsta_lr = all_lr[maxsta_lri]
+
+maxvol_lri = np.argmax(voldata[:,lambda_index])
+maxvol_lr = all_lr[maxvol_lri]
+
+
+
+
+
+maxadv_uci = np.argmax(advdata[lr_index,:])
+maxadv_uc = all_unchosen[maxadv_uci]
+
+maxsta_uci = np.argmax(stadata[lr_index,:])
+maxsta_uc = all_unchosen[maxsta_uci]
+
+maxvol_uci = np.argmax(voldata[lr_index,:])
+maxvol_uc = all_unchosen[maxvol_uci]
+
+
+
+print('best eps for common sense lr and lambda is', maxadv_eps, maxsta_eps, maxvol_eps)
+print('best lr for common sense eps and lambda is', maxadv_lr, maxsta_lr, maxvol_lr)
+print('best lambda for common sense lr and eps is', maxadv_uc, maxsta_uc, maxvol_uc)
+
+'''
---
+++ 1Simulations/2Constant-heatmaps/gridsearch_plot.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Same as 2Constant_gridsearch-plot.py script in Project1
+
+Rescorla Wagner model with epsilon-greedy decision making policy
+Constant model
+Contexts:
+*Stable: reward probabiliies stay the same during all trials
+*Volatile: reward probabilities shuffle every couple of trials
+*Reinforced variability: reward dependent on how variable options are chosen according to Hide and Seek game
+First reward schedule test:
+    stable: [0.7,0.7,0.7,0.3,0.3,0.3,0.3,0.3]
+    volatile: [0.9,0.9,0.9,0.1,0.1,0.1,0.1,0.1]
+    adversarial: least frequent 60% of sequences
+            
+Critical parameters: epsilon, learning rate and unchosen addition.
+This script will:
+Plot heatmaps used in paper
+@author: Janne Reynders; janne.reynders@ugent.be
+"""
+import numpy as np                  
+import pandas as pd                 
+import matplotlib.pyplot as plt     
+import os
+import random
+from collections import Counter
+import scipy.stats as stats
+import itertools
+import statsmodels.stats.multitest as smm
+import matplotlib
+
+
+matplotlib.rcParams['font.family'] = 'times new roman'
+matplotlib.rcParams['font.size'] = 65
+script_dir = os.path.dirname(os.path.abspath(__file__))
+folder_name = "results/reward"
+save_dir = os.path.join(script_dir, folder_name)
+
+eps0_adv_file = 'mr_var_eps0.csv'
+eps0_adv_dir = os.path.join(save_dir, eps0_adv_file)
+eps0_adv = pd.read_csv(eps0_adv_dir)
+eps0_adv=eps0_adv.to_numpy()
+eps0_adv=eps0_adv[:,3:]
+eps0_adv=eps0_adv[2:,:]
+eps0_adv=eps0_adv[::-1]
+print(np.shape(eps0_adv))
+
+eps24_adv_file = 'mr_var_eps11.csv'
+eps24_adv_dir = os.path.join(save_dir, eps24_adv_file)
+eps24_adv = pd.read_csv(eps24_adv_dir)
+eps24_adv=eps24_adv.to_numpy()
+eps24_adv=eps24_adv[:,3:]
+eps24_adv=eps24_adv[2:,:]
+eps24_adv=eps24_adv[::-1]
+
+
+eps72_adv_file = 'mr_var_eps34.csv'
+eps72_adv_dir = os.path.join(save_dir, eps72_adv_file)
+eps72_adv = pd.read_csv(eps72_adv_dir)
+eps72_adv=eps72_adv.to_numpy()
+eps72_adv=eps72_adv[:,3:]
+eps72_adv=eps72_adv[2:,:]
+eps72_adv=eps72_adv[::-1]
+
+
+
+eps0_sta_file = 'mr_sta_eps0.csv'
+eps0_sta_dir = os.path.join(save_dir, eps0_sta_file)
+eps0_sta = pd.read_csv(eps0_sta_dir)
+eps0_sta=eps0_sta.to_numpy()
+eps0_sta=eps0_sta[:,3:]
+eps0_sta=eps0_sta[2:,:]
+eps0_sta=eps0_sta[::-1]
+
+eps24_sta_file = 'mr_sta_eps11.csv'
+eps24_sta_dir = os.path.join(save_dir, eps24_sta_file)
+eps24_sta = pd.read_csv(eps24_sta_dir)
+eps24_sta=eps24_sta.to_numpy()
+eps24_sta=eps24_sta[:,3:]
+eps24_sta=eps24_sta[2:,:]
+eps24_sta=eps24_sta[::-1]
+
+eps72_sta_file = 'mr_sta_eps34.csv'
+eps72_sta_dir = os.path.join(save_dir, eps72_sta_file)
+eps72_sta = pd.read_csv(eps72_sta_dir)
+eps72_sta=eps72_sta.to_numpy()
+eps72_sta=eps72_sta[:,3:]
+eps72_sta=eps72_sta[2:,:]
+eps72_sta=eps72_sta[::-1]
+
+
+
+eps0_vol_file = 'mr_vol_eps0.csv'
+eps0_vol_dir = os.path.join(save_dir, eps0_vol_file)
+eps0_vol = pd.read_csv(eps0_vol_dir)
+eps0_vol=eps0_vol.to_numpy()
+eps0_vol=eps0_vol[:,3:]
+eps0_vol=eps0_vol[2:,:]
+eps0_vol=eps0_vol[::-1]
+print(eps0_vol)
+
+eps24_vol_file = 'mr_vol_eps11.csv'
+eps24_vol_dir = os.path.join(save_dir, eps24_vol_file)
+eps24_vol = pd.read_csv(eps24_vol_dir)
+eps24_vol=eps24_vol.to_numpy()
+eps24_vol=eps24_vol[:,3:]
+eps24_vol=eps24_vol[2:,:]
+eps24_vol=eps24_vol[::-1]
+
+eps72_vol_file = 'mr_vol_eps34.csv'
+eps72_vol_dir = os.path.join(save_dir, eps72_vol_file)
+eps72_vol = pd.read_csv(eps72_vol_dir)
+eps72_vol=eps72_vol.to_numpy()
+eps72_vol=eps72_vol[:,3:]
+eps72_vol=eps72_vol[2:,:]
+eps72_vol=eps72_vol[::-1]
+
+fontsize=70
+heat = [0,12.5,25,37.5,50]
+all_eps = np.linspace(0, 1, 51, endpoint=True)
+all_lr = [1, 0.75, 0.5, 0.25, 0]
+all_unchosen = [-1/8, -0.5/8, 0, 0.5/8, 1/8]
+cmappie = 'hot'
+title = os.path.join(save_dir, '1adveps0')
+fig, ax = plt.subplots(figsize=(28, 28))
+im = ax.imshow(eps0_adv, vmin=0, vmax=1, cmap=cmappie)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(fr"Adversarial environment" "\n" "stochasticity ($\epsilon$) = 0")
+plt.savefig(title, dpi=300, bbox_inches='tight')
+
+title = os.path.join(save_dir, '1adveps22')
+fig, ax = plt.subplots(figsize=(28, 28))
+im = ax.imshow(eps24_adv, vmin=0, vmax=1, cmap=cmappie)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Adversarial environment" "\n" "stochasticity ($\epsilon$) = 0.22")
+plt.savefig(title, dpi=300, bbox_inches='tight')
+
+title = os.path.join(save_dir, '1adveps68')
+fig, ax = plt.subplots(figsize=(28, 28))
+max_idx = np.unravel_index(np.argmax(eps72_adv), eps72_adv.shape)
+im = ax.imshow(eps72_adv, vmin=0, vmax=1, cmap=cmappie)
+ax.scatter(max_idx[1], max_idx[0], color='blue', marker='.', s=300)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Adversarial environment" "\n" "stochasticity ($\epsilon$) = 0.68")
+plt.savefig(title, dpi=300, bbox_inches='tight')
+
+
+
+
+
+title = os.path.join(save_dir, '1staeps0')
+fig, ax = plt.subplots(figsize=(28, 28))
+max_idx = np.unravel_index(np.argmax(eps0_sta), eps0_sta.shape)
+im = ax.imshow(eps0_sta, vmin=0, vmax=1, cmap=cmappie)
+ax.scatter(max_idx[1], max_idx[0], color='blue', marker='.', s=200)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Stable environment" "\n" "stochasticity ($\epsilon$) = 0")
+plt.savefig(title, dpi=300, bbox_inches='tight')
+
+title = os.path.join(save_dir, '1staeps22')
+fig, ax = plt.subplots(figsize=(28, 28))
+im = ax.imshow(eps24_sta, vmin=0, vmax=1, cmap=cmappie)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Stable environment" "\n" "stochasticity ($\epsilon$) = 0.22")
+plt.savefig(title, dpi=300, bbox_inches='tight')
+
+
+title = os.path.join(save_dir, '1staeps68')
+fig, ax = plt.subplots(figsize=(28, 28))
+im = ax.imshow(eps72_sta, vmin=0, vmax=1, cmap=cmappie)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Stable environment" "\n" "stochasticity ($\epsilon$) = 0.68")
+plt.savefig(title, dpi=300, bbox_inches='tight')
+
+
+
+
+
+
+title = os.path.join(save_dir, '1voleps0')
+fig, ax = plt.subplots(figsize=(28, 28))
+max_idx = np.unravel_index(np.argmax(eps0_vol), eps0_vol.shape)
+im = ax.imshow(eps0_vol, vmin=0, vmax=1, cmap=cmappie)
+ax.scatter(max_idx[1], max_idx[0], color='blue', marker='.', s=200)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax,fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Volatile environment" "\n" "stochasticity ($\epsilon$) = 0")
+plt.savefig(title,dpi=300, bbox_inches='tight')
+
+title = os.path.join(save_dir, '1voleps22')
+fig, ax = plt.subplots(figsize=(28, 28))
+im = ax.imshow(eps24_vol, vmin=0, vmax=1, cmap=cmappie)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax,fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Volatile environment" "\n" "stochasticity ($\epsilon$) = 0.22")
+plt.savefig(title,dpi=300, bbox_inches='tight')
+
+title = os.path.join(save_dir, '1voleps68')
+fig, ax = plt.subplots(figsize=(28, 28))
+im = ax.imshow(eps72_vol, vmin=0, vmax=1, cmap=cmappie)
+ax.set_xlabel(r'Unchosen value-bias ($\lambda$)', fontsize=fontsize)
+ax.set_ylabel(r'Learning rate ($\alpha$)', fontsize=fontsize)
+ax.set_xticks(ticks=list(heat), labels=np.round(all_unchosen,3))  
+ax.set_yticks(ticks=list(heat), labels=all_lr)  
+cbar = plt.colorbar(im, ax=ax,fraction=0.046, pad=0.04)
+cbar.set_label('Average reward')
+plt.title(r"Volatile environment" "\n" "stochasticity ($\epsilon$) = 0.68")
+plt.savefig(title,dpi=300, bbox_inches='tight')
+
+
+'''
+
+#Figure 1: REWARDS GROUPED PER EPSILON VALUE
+#plot rewards for each situation
+#first bars are grouped per epsilon value
+groups = [fr'$\epsilon$', fr'$\alpha$', fr'$\lambda$']
+categories = ['stable environment', 'volatile environment', 'adversarial environment']
+values = np.array([[0, 0, 0.72],
+                   [0.18, 0.98, 0.16],
+                   [-0.84, 0.04, 0.64]])
+
+
+category_colors = ['darkcyan', 'darkorange', 'forestgreen']
+group_colors = ['purple','red','gold']
+fig, ax = plt.subplots()
+
+bar_width = 0.2
+x = np.arange(len(groups))
+
+for j, category in enumerate(categories):
+    category_values = values[:, j]
+    
+    # Plot the bars for the category
+    for i in range(len(groups)):
+        ax.bar(x[i] + j * bar_width, category_values[i], bar_width, capsize=5, color=category_colors[j], edgecolor = group_colors[i], linewidth=1.3, alpha=0.5, label=f'{category}' if i == 0 else "")
+
+# Customize the plot
+ax.set_ylabel('Parameter value')
+plt.ylim([-1,1])
+ax.set_xticks(x + bar_width)
+ax.set_xticklabels(groups)
+#ax.legend()
+
+
+
+'''
+plt.show()
